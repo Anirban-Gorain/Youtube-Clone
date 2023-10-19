772d66128f0bba61737e0e3567d2b52de5b0baff
Anirban Gorain <gorainanirban@gmail.com>
2023-10-19 07:15:37 +0530

    Project setup is done

---
+++ main.js
@@ -0,0 +1,13 @@
+const darkModeColorScheme=
+{
+    backgroundColor: "#212121",
+    textColor: "#fafafa",
+    hoverBgc: "#424242"
+};
+
+const lightModeColorScheme=
+{
+    backgroundColor: "	#fafafa",
+    textColor: "#000000",
+    hoverBgc: "#e4e5f1"
+};
